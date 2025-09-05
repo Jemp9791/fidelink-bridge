184bb5def74cf6db2604908845f990bf8e182de4
Jemp9791 <jennyocampos@gmail.com>
2025-09-05 09:17:51 -0300

    Agregar src/utils/logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,8 @@
+/**
+ * Logger simple con timestamps ISO.
+ */
+export const log = {
+  info: (...args) => console.log(new Date().toISOString(), '[INFO]', ...args),
+  warn: (...args) => console.warn(new Date().toISOString(), '[WARN]', ...args),
+  error: (...args) => console.error(new Date().toISOString(), '[ERROR]', ...args),
+};
